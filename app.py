c5a43d3818fb91f91d4b3f6928fbfe3dcafad5df
GabrielPacco <gpaccohu@unsa.edu.pe>
2023-08-01 11:45:54 -0500

    message

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from app import create_app, socketio
 
 app = create_app()
 
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host='0.0.0.0', port=port)
