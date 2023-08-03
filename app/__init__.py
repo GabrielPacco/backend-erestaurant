23b017b7c097166d6247db870366b341512efb19
GabrielPacco <gpaccohu@unsa.edu.pe>
2023-08-03 18:18:13 -0500

    actualizando init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,18 @@
-from flask import Flask 
+from flask import Flask, render_template
 from flask_cors import CORS
-from .events import socketio
+from flask_socketio import SocketIO
 #from .routes import main 
 
+socketio = SocketIO()
+
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='templates')
     app.config["DEBUG"] = True
     app.config["SECRET_KEY"] = "secret"
+
+    @app.route('/')
+    def home():
+        return render_template('index.html')
 
     #app.register_blueprint(main)
     CORS(app, origins=['https://frontend-erestaurant-768a1a368801.herokuapp.com/'])
@@ -14,3 +20,7 @@ def create_app():
     socketio.init_app(app)
 
     return app
+
+if __name__ == "__main__":
+    app = create_app()
+    socketio.run(app)
