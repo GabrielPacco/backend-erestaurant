883b7c3b15d367a58e28e2cde230d7e60b1d08c7
GabrielPacco <gpaccohu@unsa.edu.pe>
2023-08-04 14:14:34 -0500

    el servidor inicia solo si el script se ejecuto correctamente

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import os
 from app import create_app, socketio
 
 app = create_app()
-socketio.init_app(app)
+
+if __name__ == "__main__":
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
